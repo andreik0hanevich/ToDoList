6526c04b76b3f49bfdae28e816a74f73ae4881cf
andreik0hanevich <andreikohanevich@gmail.com>
2024-08-14 17:29:22 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Todolist from "./components/Todolist";
 
 function App() {
-    return (
-        <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
-        </div>
-    );
+  const title1 = "What to learn - 1";
+  const title2 = "What to learn - 2";
+
+  const tasks1 = [
+    { id: 1, title: "HTML&CSS", isDone: true },
+    { id: 2, title: "JS", isDone: true },
+    { id: 3, title: "ReactJS", isDone: false },
+  ];
+
+  const tasks2 = [
+    { id: 1, title: "HTML&CSS", isDone: true },
+    { id: 2, title: "JS", isDone: true },
+    { id: 3, title: "ReactJS", isDone: false },
+  ];
+
+  return (
+    <div className="App">
+      <Todolist title={title1} tasks={tasks1} />
+      <Todolist title={title2} tasks={tasks2} />
+    </div>
+  );
 }
 
 export default App;
